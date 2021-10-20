20ec43a86ec641a7bed6fe8f966456f192a98499
Jason Bisson <jasonbisson@google.com>
2021-10-19 21:07:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Purpose 
+
+This repository will provide a method to create multiple Google Groups to force a conflicting account for former employees who created personal Google Identities with their corporate email address. The benefit of creating a Google group rather than a Google user identity is the following:  
+
+1. Creating an identity for former employee will create policy issues for most organizations.
+1. Avoids using a license (Cloud Identity or Workspace) for a former employee.
+
+If you have no idea what a conflict account is the best guide is found [here](https://www.goldyarora.com/google-conflicting-accounts-guide/).
+
+## Prerequisites
+
+### Install Google Client Library
+```
+pip install --upgrade google-api-python-client google-auth-oauthlib
+```
+### Create Oauth Client secret
+
+This [document](https://googleapis.github.io/google-api-python-client/docs/oauth.html) describes OAuth 2.0, when to use it, how to acquire client IDs, and how to use it with the Google APIs Client Library for Python.
+
+### Create a data file with the list of groups 
+
+```
+teraminated_employee@example.com,
+```
+
+
+## Run Script
+```
+
+```
+
+
---
+++ create_google_groups.py
@@ -0,0 +1,35 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+import csv
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/admin.directory.group']
+
+def main():
+    creds = None
+    flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    service = build('admin', 'directory_v1', credentials=creds)
+    
+    with open('groups.csv', "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            group = { "name": row[0], "email": row[0], "adminCreated": True, "description": row[0]}
+            results = service.groups().insert(body=group).execute()
+            print(results["email"])
+
+if __name__ == '__main__':
+    main()
